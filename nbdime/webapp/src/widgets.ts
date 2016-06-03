12456f830563ccffcd6ed3775b8fbb595a395290
Vidar Tonaas Fauske <vidartf@users.noreply.github.com>
2016-06-03 11:43:39 +0200

    Add collapsible widget for outputs/metadata view

--- nbdime/webapp/src/widgets.ts
+++ nbdime/webapp/src/widgets.ts
@@ -84,6 +84,78 @@ const EDITOR_CLASS = 'jp-Editor';
  */
 const CODEMIRROR_CLASS = 'jp-CodeMirror';
 
+
+const COLLAPISBLE_HEADER = 'jp-Collapsible-header';
+const COLLAPISBLE_HEADER_ICON = 'jp-Collapsible-header-icon';
+const COLLAPISBLE_HEADER_ICON_OPEN = 'jp-Collapsible-header-icon-opened';
+const COLLAPISBLE_HEADER_ICON_CLOSED = 'jp-Collapsible-header-icon-closed';
+const COLLAPISBLE_SLIDER = 'jp-Collapsible-slider';
+const COLLAPSIBLE_OPEN = 'jp-Collapsible-opened';
+const COLLAPSIBLE_CLOSED = 'jp-Collapsible-closed';
+const COLLAPSIBLE_CONTAINER = 'jp-Collapsible-container';
+
+
+
+/**
+ * CollapsiblePanel
+ */
+class CollapsibleWidget extends Widget {
+    static createHeader(headerTitle?: string): HTMLSpanElement {
+        let header = document.createElement("div");
+        header.className = COLLAPISBLE_HEADER;
+        if (headerTitle) {
+            //let title = document.createElement("span");
+            header.innerText = headerTitle;
+            //header.appendChild(title);
+        }
+        let button = document.createElement("span");
+        button.className = COLLAPISBLE_HEADER_ICON;
+        header.appendChild(button)
+        
+        return header;
+    }
+    
+    constructor(public inner: Widget, headerTitle?: string, collapsed?: boolean) {
+        super();
+        let constructor = this.constructor as typeof CollapsibleWidget;
+        let header = constructor.createHeader(headerTitle);
+        this.button = header.getElementsByClassName(COLLAPISBLE_HEADER_ICON)[0] as HTMLElement;
+        header.onclick = this.toggleCollapsed.bind(this);
+        this.node.appendChild(header);
+        this.container = document.createElement("div");
+        this.container.className = COLLAPSIBLE_CONTAINER;
+        this.slider = document.createElement("div");
+        this.slider.classList.add(COLLAPISBLE_SLIDER);
+        this.slider.appendChild(inner.node)
+        this.container.appendChild(this.slider);
+        this.node.appendChild(this.container);
+        
+        this.slider.classList.add(collapsed === true ? COLLAPSIBLE_CLOSED : COLLAPSIBLE_OPEN);
+        this.button.classList.add(collapsed === true ? COLLAPISBLE_HEADER_ICON_CLOSED : COLLAPISBLE_HEADER_ICON_OPEN);
+    }
+    
+    toggleCollapsed(): void {
+        let slider = this.slider;
+        if (this.collapsed) {
+            slider.classList.remove(COLLAPSIBLE_CLOSED);
+            slider.classList.add(COLLAPSIBLE_OPEN);
+            
+        } else {
+            slider.classList.remove(COLLAPSIBLE_OPEN);
+            slider.classList.add(COLLAPSIBLE_CLOSED);
+        }
+    }
+    
+    get collapsed(): boolean {
+        return this.slider.classList.contains(COLLAPSIBLE_CLOSED);
+    }
+    
+    slider: HTMLElement;
+    container: HTMLElement;
+    button: HTMLElement;
+}
+
+
 /**
  * NotebookDiffWidget
  */
@@ -143,6 +215,17 @@ class CellDiffWidget extends Panel {
     static createView(model: IDiffViewModel, editorClasses: string[]): NbdimeMergeView {
         return new NbdimeMergeView(model, editorClasses);
     }
+    
+    
+    /**
+     * Create a new collapsible view.
+     */
+    static createCollapsibleView(model: IDiffViewModel, editorClasses: string[],
+            header?: string, collapsed?: boolean): CollapsibleWidget {
+        let view = new NbdimeMergeView(model, editorClasses);
+        let collapser = new CollapsibleWidget(view, header, collapsed);
+        return collapser;
+    }
 
     /**
      * 
@@ -183,27 +266,24 @@ class CellDiffWidget extends Panel {
         // Add inputs and outputs, on a row-by-row basis
         let sourceView = constructor.createView(model.sourceView, CURR_DIFF_CLASSES);
         sourceView.addClass(SOURCE_ROW_CLASS);
-        this.addView(sourceView);
+        this.addWidget(sourceView);
         
         if (model.metadata && !model.metadata.unchanged) {
-            let metadataView = constructor.createView(model.metadataView, CURR_DIFF_CLASSES);
-            metadataView.addClass(METADATA_ROW_CLASS);
-            this.addView(metadataView);
+            let metadataCollapser = constructor.createCollapsibleView(
+                model.metadataView, CURR_DIFF_CLASSES, "Metadata changed", true);
+            metadataCollapser.addClass(METADATA_ROW_CLASS);
+            this.addWidget(metadataCollapser);
         }
         if (model.outputs && !model.outputs.unchanged) {
-            let outputsView = constructor.createView(model.outputsView, CURR_DIFF_CLASSES);
+            let outputsView = constructor.createCollapsibleView(
+                model.outputsView, CURR_DIFF_CLASSES, "Output changed", false);
             outputsView.addClass(OUTPUTS_ROW_CLASS);
-            this.addView(outputsView);
+            this.addWidget(outputsView);
         }
     }
     
     public addWidget(widget: Widget) {
         (this.layout as PanelLayout).addChild(widget);
-    }
-    
-    public addView(view: NbdimeMergeView) {
-        this._views.push(view);
-        (this.layout as PanelLayout).addChild(view);
     }
     
     /**
@@ -216,13 +296,8 @@ class CellDiffWidget extends Panel {
         return this._model;
     }
     
-    get views(): NbdimeMergeView[] {
-        return this._views;
-    }
-    
     protected _model: ICellDiffModel = null;
     protected _rendermime: RenderMime<Widget> = null;
-    protected _views: NbdimeMergeView[] = [];
 }
 
 /**
@@ -246,9 +321,6 @@ class NbdimeMergeView extends Widget {
         }
         if (this._mergeview.merge) {
             this._editors.push(this._mergeview.merge);
-        }
-        for (var edt of this._editors) {
-           
         }
     }
     
